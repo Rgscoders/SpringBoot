3070bc4e8da9b2ec45c9b39c37de5b463a45b778
manoharreddyadika <manohar.22369@gmail.com>
2023-12-06 10:37:22 +0530

    updated code UserServiceImplTest
    updated code UserServiceImplTest

--- springpractice-1/src/test/java/com/rgs/service/impl/UserServiceImplTest.java
+++ springpractice-1/src/test/java/com/rgs/service/impl/UserServiceImplTest.java
@@ -1,6 +1,13 @@
 package com.rgs.service.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doNothing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,4 +53,78 @@ public class UserServiceImplTest {
 		
 		
 	}
+	
+	@Test
+	public void loadUsersTest() throws Exception{
+		List<UserEntity> users=new ArrayList<>();
+		UserEntity user1=new UserEntity();
+		UserEntity user2=new UserEntity();
+		users.add(user1);
+		users.add(user2);
+		Mockito.when(userRepository.findAll()).thenReturn(users);
+		List<UserDTO> usersList=userServiceImpl.loadUsers();
+		assertNotNull(usersList);
+		assertEquals(2, usersList.size());
+		
+	}
+	@Test(expected=MyCustomException.class)
+	public void loadUsersTestException() throws Exception{
+		Mockito.when(userRepository.findAll()).thenThrow(NullPointerException.class);
+		userServiceImpl.loadUsers();
+		
+	}
+	@Test
+	public void loadUserTest() throws Exception{
+		Integer userId=1;
+		UserEntity userEntity=new UserEntity();
+		Optional<UserEntity> optional=Optional.of(userEntity);
+		Mockito.when(userRepository.findById(userId)).thenReturn(optional);
+		UserDTO user=userServiceImpl.loadUser(userId);
+		assertNotNull(user);
+		
+	}
+	@Test(expected=MyCustomException.class)
+	public void loadUserTextException() throws Exception{
+		Integer userId=1;
+		Mockito.when(userRepository.findById(userId)).thenThrow(NullPointerException.class);
+		userServiceImpl.loadUser(userId);
+		
+	}
+	@Test
+	public void updateUserTest() throws Exception{
+		UserDTO userDTO=new UserDTO();
+		userDTO.setUserId(1);
+		UserEntity userEntity=new UserEntity();
+		userEntity.setUserId(1);
+		Mockito.when(userRepository.save(Mockito.any())).thenReturn(userEntity);
+		userServiceImpl.updateUser(userDTO);
+		assertTrue(true);
+		
+	}
+	@Test(expected=MyCustomException.class)
+	public void updateUserTestException() throws Exception{
+		Mockito.when(userRepository.save(Mockito.any())).thenThrow(NullPointerException.class);
+		userServiceImpl.updateUser(new UserDTO());
+		
+		
+	}
+	@Test
+	public void deleteUserTest() throws Exception{
+		Integer userId=1;
+		UserEntity userEntity=new UserEntity();
+		Optional<UserEntity> optional=Optional.of(userEntity);
+		Mockito.when(userRepository.findById(userId)).thenReturn(optional);
+		doNothing().when(userRepository).delete(Mockito.any());
+		userServiceImpl.deleteUser(userId);
+		assertTrue(true);
+		
+	}
+	@Test(expected=MyCustomException.class)
+	public void deleteUserTestException() throws Exception{
+		Integer userId=1;
+		Mockito.when(userRepository.findById(userId)).thenThrow(NullPointerException.class);
+		userServiceImpl.deleteUser(userId);
+		
+	}
+
 }
